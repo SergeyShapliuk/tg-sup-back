34c4bab4c57ddaa8a6cde129b2ec5d40e4369af6
shapl_xlbobtq <sergeshapluk@gmail.com>
2026-02-02 17:46:15 +0300

    fix bot settings

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -1,5 +1,5 @@
 export const SETTINGS = {
-  PORT: process.env.PORT || 5001,
+  PORT: process.env.PORT || 3000,
   MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/mongo',
   DB_NAME: process.env.DB_NAME || 'mongo',
   AMOUNT: 100,
